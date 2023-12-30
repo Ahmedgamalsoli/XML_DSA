a5a7f5f83be4c417bc215ef01b8c47d2ab7300a8
aKha1ifa <131745823+aKha1ifa@users.noreply.github.com>
2023-12-30 15:14:04 +0200

    Create errorChecking.h

---
+++ errorChecking.h
@@ -0,0 +1,9 @@
+#ifndef DSA_PROJECT_ERRORCHECKING_H
+#define DSA_PROJECT_ERRORCHECKING_H
+#include<string>
+#include<vector>
+
+using namespace std ;
+
+bool errorChecking(const string& xmlFilePath,string & errorMessages,string &beginminus);
+#endif //DSA_PROJECT_ERRORCHECKING_H
