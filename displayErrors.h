7365c2b2fd957ea1d33b150bd4458c5a3f14ed91
Seif-Gama1 <126584186+Seif-Gama1@users.noreply.github.com>
2023-12-30 15:05:29 +0200

    Create displayErrors.h

---
+++ displayErrors.h
@@ -0,0 +1,10 @@
+#ifndef DSA_PROJECT_DISPLAYERRORS_H
+#define DSA_PROJECT_DISPLAYERRORS_H
+#include <string>
+#include <vector>
+using namespace  std ;
+
+
+vector<int> displayErrors(const string& aux_xmlFilePath ,const string& RealxmlFilePath );
+
+#endif //DSA_PROJECT_DISPLAYERRORS_H
