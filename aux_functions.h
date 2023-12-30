3bae1cc055049b41cab10c7c052820e00cf6cc04
Ahmedgamalsoli <121308198+Ahmedgamalsoli@users.noreply.github.com>
2023-12-30 17:54:56 +0200

    Add files via upload

---
+++ aux_functions.h
@@ -0,0 +1,12 @@
+//
+// Created by seif alrahman on 12/30/2023.
+//
+
+#ifndef DSA_PROJECT_AUX_FUNCTIONS_H
+#define DSA_PROJECT_AUX_FUNCTIONS_H
+#include <string>
+#include <vector>
+using namespace std ;
+string removeSpaces(const std::string& str);
+bool areLinesIdentical(const std::string& line1, const std::string& line2);
+#endif //DSA_PROJECT_AUX_FUNCTIONS_H
