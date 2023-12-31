53e12f95559d4d0c9e2e16c003dc61640339b372
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2023-12-31 09:43:24 +0200

    Add files via upload

---
+++ errorCorrecting.h
@@ -0,0 +1,12 @@
+//
+// Created by seif alrahman on 12/30/2023.
+//
+
+#ifndef DSA_PROJECT_ERRORCORRECTING_H
+#define DSA_PROJECT_ERRORCORRECTING_H
+#include<string>
+#include<vector>
+
+using namespace std ;
+vector<string> errorCorrecting (const string& xmlFilePath,string &errorMessages,string &beginminus) ;
+#endif //DSA_PROJECT_ERRORCORRECTING_H
